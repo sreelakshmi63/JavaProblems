0bd3abcd28530f43b6aa0a11e26b0dc6754531ff
sreelakshmi63 <sreelakshmiajith95@gmail.com>
2021-05-05 01:47:46 +0530

    corrected entire code logic

--- String/LongestSubstring.java
+++ String/LongestSubstring.java
@@ -1,14 +1,12 @@
-
-import java.util.HashMap;
+import java.util.ArrayList;
 
 //Longest substring without repeating characters
 public class LongestSubstring {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str = "pwwkew";
+		String str = "dvdf";
 		int res = longestSubstring(str);
-		System.out.println(str);
 		System.out.println(res);
 		
 		
@@ -16,21 +14,24 @@ public class LongestSubstring {
 	
 	public static int longestSubstring(String str){
 		int res = 0;
-		int counter = 0;
-		HashMap<Integer, Integer> lmap = new HashMap<Integer,Integer>();
-		for(int i = 0; i < str.length(); i++){
-			if(!lmap.containsKey((int)str.charAt(i))){
-				counter++;
-				lmap.put((int)str.charAt(i), 0);
-			}else{
-				lmap = new HashMap<Integer,Integer>();
-				lmap.put((int)str.charAt(i), 0);
-				counter = 1;
-				
+		if(str.length() > 0){
+			ArrayList<Integer> larr = new ArrayList<Integer>();
+			//char fa = str.charAt(0);
+			int counter = 0;
+			for(int i=0;i<str.length();i++){
+				if(!larr.contains((int)str.charAt(i))){
+					larr.add((int)str.charAt(i));
+					counter++;
+				}else{
+					break;
+				}
 			}
-			res = Math.max(res, counter);
+			res =  Math.max(counter, longestSubstring(str.substring(1, str.length())));
+		}else{
+			res = -1;
 		}
 		return res;
+		
 	}
 
 }
